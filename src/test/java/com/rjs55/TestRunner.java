83cf92bc37d81ae4a28da641446a003742d3f49a
richardsuller <rsuller@sky.com>
2016-12-16 16:32:54 +0000

    Added support for reporting from https://github.com/email2vimalraj/CucumberExtentReporter and Extent report http://extentreports.relevantcodes.com

--- src/test/java/com/rjs55/TestRunner.java
+++ src/test/java/com/rjs55/TestRunner.java
@@ -1,13 +1,39 @@
 package com.rjs55;
 
+import com.cucumber.listener.ExtentCucumberFormatter;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "html:target/cucumber"},
+@CucumberOptions(plugin = {"pretty", "com.cucumber.listener.ExtentCucumberFormatter"},
         monochrome = true)
 
 
 public class TestRunner {
+
+    @BeforeClass
+    public static void setup() {
+        // Initiates the extent report and generates the output in the output/Run_<unique timestamp>/report.html file by default.
+        ExtentCucumberFormatter.initiateExtentCucumberFormatter();
+
+        // Loads the extent config xml to customize on the report.
+        ExtentCucumberFormatter.loadConfig(new File("src/test/resources/extent-config.xml"));
+
+        // User can add the system information as follows
+        ExtentCucumberFormatter.addSystemInfo("Browser Name", "Firefox");
+        ExtentCucumberFormatter.addSystemInfo("Browser version", "v31.0");
+        ExtentCucumberFormatter.addSystemInfo("Selenium version", "v2.53.0");
+
+        // Also you can add system information using a hash map
+        Map systemInfo = new HashMap();
+        systemInfo.put("Cucumber version", "v1.2.3");
+        systemInfo.put("Extent Cucumber Reporter version", "v1.1.1");
+        ExtentCucumberFormatter.addSystemInfo(systemInfo);
+    }
 }
